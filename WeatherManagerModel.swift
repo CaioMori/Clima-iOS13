322df8382c4382385ca425f33eb5c4b8da21548e
Caio Mori <caiomori22@icloud.com>
2023-08-03 18:53:04 -0300

    feat(WeatherManagerModel): add fetch with environment apikey

--- WeatherManagerModel.swift
+++ WeatherManagerModel.swift
@@ -9,10 +9,37 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherURL = "http://api.openweathermap.org/data/2.5/weather?APPID=2b5429bc671d29a573dfdcc9cde3aa3f&units=metric"
+    private let apiKey = ProcessInfo.processInfo.environment["API_KEY"] ?? ""
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather"
     
     func fetchWeather(cityName: String) {
-        let urlString = "\(weatherURL)&q=\(cityName)"
+        if let encodedCityName = cityName.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
+                let urlString = "\(weatherURL)?APPID=\(apiKey)&q=\(encodedCityName)&units=metric"
+                performRequest(urlString: urlString)
+            }
     }
     
+    
+    func performRequest(urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url, completionHandler: handle)
+            
+            task.resume()
+        }
+        
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) -> Void {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString!)
+        }
+    }
 }
